5435f7a99cf6de1c4c72693d5f5e3b9dac1b7b3c
2938234632@qq.com <2938234632@qq.com>
2019-05-20 17:21:33 +0800

    feat：注册需求代码封装

--- Mooc_TestProject/start_browser.py
+++ Mooc_TestProject/start_browser.py
@@ -1,36 +1,40 @@
 # coding=utf-8
 from selenium import webdriver
-driver = webdriver.Chrome()
 import time
 import random
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+# driver = webdriver.Chrome()
 
-driver.get("http://www.5itest.cn/register")
+# driver.get("http://www.5itest.cn/register")
 time.sleep(5)
-# 使用title_contains检查页面是否正确
-print(EC.title_contains("注册"))
-email_element = driver.find_element_by_id("register_email")
+# # 使用title_contains检查页面是否正确
+# print(EC.title_contains("注册"))
+# email_element = driver.find_element_by_id("register_email")
+
+for i in range(5):
+    user_email = ''.join(random.sample("1234567890abcdefg", 5)) + '@163.com'
+    print(user_email)
 
 
-element = driver.find_element_by_class_name("controls")
-locator = (By.CLASS_NAME, "controls")
-'''
-使用Excepted_conditions判断元素是否可见
-element = driver.find_element_by_class_name("controls")
-EC.visibility_of_element_located(element) 判断传入的元素是否可见
-'''
-WebDriverWait(driver, 1).until(EC.visibility_of_element_located(locator))
-'''
-输入注册用户名及获取用户信息
-get_attribute()获取用户属性
-'''
-email_element = driver.find_element_by_id("register_email")
-print(email_element.get_attribute("placeholder"))
-email_element.send_keys("test123@163.com")
-print(email_element.get_attribute("value"))
-driver.close()
+# element = driver.find_element_by_class_name("controls")
+# locator = (By.CLASS_NAME, "controls")
+# '''
+# 使用Excepted_conditions判断元素是否可见
+# element = driver.find_element_by_class_name("controls")
+# EC.visibility_of_element_located(element) 判断传入的元素是否可见
+# '''
+# WebDriverWait(driver, 1).until(EC.visibility_of_element_located(locator))
+# '''
+# 输入注册用户名及获取用户信息
+# get_attribute()获取用户属性
+# '''
+# email_element = driver.find_element_by_id("register_email")
+# print(email_element.get_attribute("placeholder"))
+# email_element.send_keys("test123@163.com")
+# print(email_element.get_attribute("value"))
+# driver.close()
 
 # driver.find_element_by_id("register_email").send_keys("glp123@163.com")
 # # 子父节点定位  find_elements_by_class_name
@@ -39,4 +43,3 @@ driver.close()
 # user_element.send_keys("glp123")
 # driver.find_element_by_name("password").send_keys("11111111")
 # driver.find_element_by_xpath("//*[@id='captcha_code']").send_keys("1111")
-# driver.close()
