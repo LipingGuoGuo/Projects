3aef9642e143b2eab68b28569e52b70c80cde008
2938234632@qq.com <2938234632@qq.com>
2019-05-20 17:44:34 +0800

    feat：PO模式

---
+++ Mooc_TestProject/business/regidter_business.py
@@ -0,0 +1,6 @@
+"""
+操作handle层
+
+"""
+
+class RegisterBusiness(object):
---
+++ Mooc_TestProject/case/first_case.py
@@ -0,0 +1,13 @@
+class FirstCase(object):
+    def test_login_email_error(self):
+        login("34", "111")
+        # 通过assert判断是否为error
+
+    def test_login_username_error(self):
+        login("34", "111")
+
+    def test_login_code_error(self):
+        login("34", "111")
+
+    def test_login_success(self):
+        login("34", "111")
