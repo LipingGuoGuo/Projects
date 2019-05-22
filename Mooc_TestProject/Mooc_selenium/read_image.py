2a79ca9e09b437d9ef22de76cb7a8fb7cc47456d
2938234632@qq.com <2938234632@qq.com>
2019-05-22 18:26:56 +0800

    feat：导入pytesseract库，获取图片文字

--- Mooc_TestProject/Mooc_selenium/read_image.py
+++ Mooc_TestProject/Mooc_selenium/read_image.py
@@ -1,15 +1,19 @@
 #codinng=utf-8
 import pytesseract
 from PIL import Image
-from ShowapiRequest import ShowapiRequest
-#image = Image.open("E:/imooc2.png")
-#text = pytesseract.image_to_string(image)
-#print(text)
+# from ShowapiRequest import ShowapiRequest
 
-r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
-r.addBodyPara("typeId", "35")
-r.addBodyPara("convert_to_jpg", "0")
-r.addFilePara("image", r"E:/imooc1.png") #文件上传时设置
-res = r.post()
-text = res.json()['showapi_res_body']['Result']
-print(text) # 返回信息
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tessdata"'
+
+image = Image.open("F:/timg.gif")
+text = pytesseract.image_to_string(image, config=tessdata_dir_config)
+print(text)
+
+# r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
+# r.addBodyPara("typeId", "35")
+# r.addBodyPara("convert_to_jpg", "0")
+# r.addFilePara("image", r"E:/imooc1.png") #文件上传时设置
+# res = r.post()
+# text = res.json()['showapi_res_body']['Result']
+# print(text) # 返回信息
