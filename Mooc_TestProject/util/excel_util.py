44c4ec70f9b2cc2f97a7f387d754dcaaab3ee89b
LipingGuoGuo <2938234632@qq.com>
2019-05-28 23:57:47 +0800

    msg:增加写入数据获取单元格，增加容错判断

--- Mooc_TestProject/util/excel_util.py
+++ Mooc_TestProject/util/excel_util.py
@@ -17,21 +17,32 @@ class ExcelUtil:
     # 获取excel数据，按照每行一个list,添加到一个大的list里面
     def get_data(self):
         result = []
-        for i in range(self.get_lines()):
-            col = self.table.row_values(i)
-            result.append(col)
-        return result
+        rows = self.get_lines()
+        if rows !=None:
+            for i in range(rows):
+                col = self.table.row_values(i)
+                result.append(col)
+            return result
+        return None
     # 获取excel行数
     def get_lines(self):
         # 行数
         rows = self.table.nrows
-        return rows
+        if rows>= 1:
+            return rows
+        return None
+
     # 获取单元格的数据
-    def get_col_value(self):
-        data = self.table.cell(4, 4).value
-        return data
+    def get_col_value(self, row, col):
+        if self.get_lines()>row:
+            data = self.table.cell(4, 4).value
+            return data
+
+    # 判断行数
+    def has_next(self):
+
     # 写入数据
-    def write_value(self,row,value):
+     def write_value(self,row,value):
         read_value = self.data
         write_data = copy(read_value)
         write_data.get_sheet(0).write(row, 7, value)
@@ -39,5 +50,5 @@ class ExcelUtil:
 
 if __name__ == "__main__":
     ex = ExcelUtil("F:\Projects\Projects\Mooc_TestProject\config\keyword2.xls")
-    print(ex.write_value(7, "test"))
+    print(ex.get_col_value(10, 10))
 
