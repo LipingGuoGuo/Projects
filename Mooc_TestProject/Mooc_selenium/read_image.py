f3246a1a50e49a5bf4f9cd331e573a7deadd859e
2938234632@qq.com <2938234632@qq.com>
2019-05-29 18:35:38 +0800

    feat：更新showapirequest

--- Mooc_TestProject/Mooc_selenium/read_image.py
+++ Mooc_TestProject/Mooc_selenium/read_image.py
@@ -1,19 +1,21 @@
-#codinng=utf-8
+#coding=utf-8
 import pytesseract
 from PIL import Image
 from ShowapiRequest import ShowapiRequest
 
 # pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
 # tessdata_dir_config = '--tessdata-dir "C:/Program Files/Tesseract-OCR/tessdata"'
-
-# image = Image.open("F:/timg.gif")
+#
+# image = Image.open("F:/test.png")
 # text = pytesseract.image_to_string(image, config=tessdata_dir_config)
 # print(text)
 
-r = ShowapiRequest("http://route.showapi.com/184-4","62626","d61950be50dc4dbd9969f741b8e730f5" )
+r = ShowapiRequest("http://route.showapi.com/184-5", "96517", "213a7970dd72492aaaee595c9d64f7d9" )
+r.addBodyPara("img_base64", "")
 r.addBodyPara("typeId", "35")
 r.addBodyPara("convert_to_jpg", "0")
-r.addFilePara("image", r"E:/imooc1.png") #文件上传时设置
+r.addBodyPara("needMorePrecise", "0")
+r.addFilePara("image", r"F:/imooc1.png")  # 文件上传时设置
 res = r.post()
-text = res.json()['showapi_res_body']['Result']
-print(text) # 返回信息
+# text = res.json()["showapi_res_body"]["Result"]
+print(res.text)  # 返回信息
--- Mooc_TestProject/util/excel_util.py
+++ Mooc_TestProject/util/excel_util.py
@@ -3,6 +3,7 @@
 """
 import xlrd
 from xlutils.copy import copy
+
 class ExcelUtil:
     def __init__(self, excel_path=None, index=None):
         if excel_path == None:
@@ -14,27 +15,29 @@ class ExcelUtil:
         # # 行数
         # self.rows = self.table.nrows
         # [[],[]]
+
     # 获取excel数据，按照每行一个list,添加到一个大的list里面
     def get_data(self):
         result = []
         rows = self.get_lines()
-        if rows !=None:
+        if rows != None:
             for i in range(rows):
                 col = self.table.row_values(i)
                 result.append(col)
             return result
         return None
+
     # 获取excel行数
     def get_lines(self):
         # 行数
         rows = self.table.nrows
-        if rows>= 1:
+        if rows >= 1:
             return rows
         return None
 
     # 获取单元格的数据
     def get_col_value(self, row, col):
-        if self.get_lines()>row:
+        if self.get_lines() > row:
             data = self.table.cell(4, 4).value
             return data
 
@@ -42,13 +45,13 @@ class ExcelUtil:
     def has_next(self):
 
     # 写入数据
-     def write_value(self,row,value):
+     def write_value(self, row, value):
         read_value = self.data
         write_data = copy(read_value)
         write_data.get_sheet(0).write(row, 7, value)
         write_data.save("F:\Projects\Projects\Mooc_TestProject\config\keyword2.xls")
 
+
 if __name__ == "__main__":
     ex = ExcelUtil("F:\Projects\Projects\Mooc_TestProject\config\keyword2.xls")
     print(ex.get_col_value(10, 10))
-
